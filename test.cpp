f1536b4f5eb9f385b55cc67d970506f1bee9b9ec
Mode <mode89@mail.ru>
2016-07-30 15:14:45 +0800

    create window, opengl context and main loop

---
+++ test.cpp
@@ -0,0 +1,41 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+
+#define DEBUG(...) \
+    printf(__VA_ARGS__); \
+    printf("\n");
+
+#define VGL(func, ...) \
+    func(__VA_ARGS__); \
+    if (glGetError() != GL_NONE) \
+    { \
+        DEBUG("Failed in " #func "()"); \
+    }
+
+int main()
+{
+    DEBUG("Initializing GLFW ...");
+    glfwInit();
+
+    DEBUG("Creating window ...");
+    GLFWwindow * window = glfwCreateWindow(
+        640, 480, "Triangulation", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    DEBUG("Initializing GLEW ...");
+    glewInit();
+
+    DEBUG("Running ...");
+    while (!glfwWindowShouldClose(window))
+    {
+        VGL(glClearColor, 0.0f, 0.0f, 0.3f, 1.0f);
+        VGL(glClear, GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
